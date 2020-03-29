6e5b6fd4a62a78379cd29d19369b2ebf7549cf77
alex <alextotalk@gmail.com>
2020-03-30 02:51:49 +0300

    Updates

---
+++ precache-manifest.3fa5fbb94e47351433e75a39c5ef1ec6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8e0a6d8724dc6c9da863e2e2208b49",
+    "url": "/maski/index.html"
+  },
+  {
+    "revision": "557cb12be2ad61201ba9",
+    "url": "/maski/static/css/main.05a24dff.chunk.css"
+  },
+  {
+    "revision": "a2ab97df9a64c0c837ad",
+    "url": "/maski/static/js/2.15f1aaab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maski/static/js/2.15f1aaab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557cb12be2ad61201ba9",
+    "url": "/maski/static/js/main.bccb4a58.chunk.js"
+  },
+  {
+    "revision": "769b8a3c410d0aeb717f",
+    "url": "/maski/static/js/runtime-main.9f23d203.js"
+  },
+  {
+    "revision": "5b654ca3ff43518f2dca4ddce9fbcc69",
+    "url": "/maski/static/media/1.5b654ca3.jpg"
+  },
+  {
+    "revision": "38194885a1ddf7cba40c3f9c44520a32",
+    "url": "/maski/static/media/2.38194885.jpg"
+  },
+  {
+    "revision": "d0b46f152e9008f2ce74fec334abcda0",
+    "url": "/maski/static/media/3.d0b46f15.jpg"
+  },
+  {
+    "revision": "e8cbec71789928477698807c3bb58f69",
+    "url": "/maski/static/media/4.e8cbec71.jpg"
+  }
+]);
